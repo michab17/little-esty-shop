2378a871c4c67cf8371c6a300918d4f5cba67662
Micha Bernhard <mib70@pitt.edu>
2021-11-13 10:50:01 -0700

    Test: write test for user story 3

--- spec/features/merchant_discounts/index_spec.rb
+++ spec/features/merchant_discounts/index_spec.rb
@@ -7,12 +7,28 @@ RSpec.describe 'Merchant Discount Index Page' do
     discount2 = Discount.create!(merchant_id: merchant.id, percentage: 0.50, quantity: 5)
 
     visit merchant_discounts_path(merchant)
-    save_and_open_page
+
     expect(page).to have_content("Discount #1")
     expect(page).to have_content("Percentage: 25%")
     expect(page).to have_content("Quantity: 3")
     expect(page).to have_content("Discount #2")
     expect(page).to have_content("Percentage: 50%")
     expect(page).to have_content("Quantity: 5")
+  end
+
+  it 'has a link to create a new discount' do
+    merchant = Merchant.create!(name: 'Johnny Create')
+
+    visit merchant_discounts_path(merchant)
+
+    click_link 'Create Discount'
+
+    fill_in :percentage, with: 0.25
+    fill_in :quantity, with: 5
+    click_button 'Create'
+
+    expect(current_path).to eq(merchant_discounts_path(merchant))
+    expect(page).to have_content('25%')
+    expect(page).to have_content('5')
   end
 end
