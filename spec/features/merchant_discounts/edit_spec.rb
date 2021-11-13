47d0771414d076cf38f6bf9f7f98196cb2ebf712
Micha Bernhard <mib70@pitt.edu>
2021-11-13 13:00:32 -0700

    Test: write test for user story 6

---
+++ spec/features/merchant_discounts/edit_spec.rb
@@ -0,0 +1,25 @@
+require 'rails_helper'
+
+RSpec.describe 'Merchant Discounts edit page' do
+  it 'allows the merchant to edit discounts' do
+    merchant = Merchant.create!(name: 'Freddy Kruger')
+    discount1 = Discount.create!(merchant_id: merchant.id, percentage: 0.25, quantity: 3)
+
+    visit merchant_discount_path(merchant, discount1)
+
+    click_link 'Edit Discount'
+
+    expect(page).to have_content('25%')
+    expect(page).to have_content('3')
+
+    fill_in :percentage, with: .75
+    fill_in :quantity, with: 75
+    click_button 'Update'
+
+    expect(current_path).to eq(merchant_discount_path(merchant, discount1))
+    expect(page).to have_content('75%')
+    expect(page).to have_content('75')
+    expect(page).to_not have_content('25%')
+    expect(page).to_not have_content('3')
+  end
+end
