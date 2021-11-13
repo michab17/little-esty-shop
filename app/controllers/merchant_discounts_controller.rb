7391ce619c7eb9e605d89a3d46c16c6219cacb4c
Micha Bernhard <mib70@pitt.edu>
2021-11-13 11:52:38 -0700

    Add: percentage field now needs to be less than 1

--- app/controllers/merchant_discounts_controller.rb
+++ app/controllers/merchant_discounts_controller.rb
@@ -9,8 +9,13 @@ class MerchantDiscountsController < ApplicationController
   
   def create
     merchant = Merchant.find(params[:merchant_id])
-    merchant.discounts.create!(discount_params)
-    redirect_to merchant_discounts_path(merchant)
+    if discount_params[:percentage].to_f >= 1
+      require 'pry'; binding.pry
+      redirect_to new_merchant_discount_path(merchant), notice: 'Perchantage must be entered as a decimal value less than one'
+    else
+      merchant.discounts.create!(discount_params)
+      redirect_to merchant_discounts_path(merchant)
+    end
   end
 
   private
