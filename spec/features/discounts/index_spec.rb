829dd51b5b7122a0dea4626d416871982686f6cc
Micha Bernhard <mib70@pitt.edu>
2021-11-12 18:53:54 -0700

    stub API response

--- spec/features/discounts/index_spec.rb
+++ spec/features/discounts/index_spec.rb
@@ -7,7 +7,7 @@ RSpec.describe 'Discounts Index Page' do
     within "#upcoming_holidays" do
       expect(page).to have_content('Thanksgiving Day')
       expect(page).to have_content('Christmas Day')
-      expect(page).to have_content("New Year's Day")
+      expect(page).to have_content("New Years Day")
       expect(page).to_not have_content("Martin Luther King, Jr. Day")
     end
   end
