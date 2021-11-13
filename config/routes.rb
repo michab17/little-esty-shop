9577ab1b3f168bd7a5e2a745adafab4aa8b8de6c
Micha Bernhard <mib70@pitt.edu>
2021-11-12 17:22:34 -0700

    Add: implement user story 1

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  # resources :merchant_dashboards, only: [:index]
-  # resources :merchant_items,      only: [:index, :new, :create, :show]
-  # resources :merchant_invoices,   only: [:index, :show]
   get "/merchants", to: "merchants#index"
 
   get "/merchants/:merchant_id/dashboard", to: "merchant_dashboards#index"
@@ -16,8 +13,6 @@ Rails.application.routes.draw do
   patch "/merchants/:merchant_id/items/:item_id", to: "merchant_items#update"
 
   get "/merchants/:merchant_id/invoices", to: "merchant_invoices#index"
-
-
 
   patch "/merchants/:merchant_id/invoices/:invoice_id", to: "merchant_invoices#update"
   get "/merchants/:merchant_id/invoices/:invoice_id", to: "merchant_invoices#show"
@@ -37,8 +32,8 @@ Rails.application.routes.draw do
   get '/admin/invoices', to: 'admin_invoices#index'
   get '/admin/invoices/:id', to: 'admin_invoices#show', as: :admin_invoices_show
   patch '/admin/invoices/:id', to: 'admin_invoices#update'
-  # namespace :admin do
-  #   resources :merchants, only: [:index]
-  #   resources :invoices, only: [:index]
-  # end
+
+  scope '/merchants/:merchant_id' do
+    resources :discounts, only: [:index], controller: 'merchant_discounts', as: :merchant_discounts
+  end
 end
