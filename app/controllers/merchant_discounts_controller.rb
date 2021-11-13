35d5e4b8541c6e8190be67934eb99801b996e999
Micha Bernhard <mib70@pitt.edu>
2021-11-13 11:54:04 -0700

    Delete: pry

--- app/controllers/merchant_discounts_controller.rb
+++ app/controllers/merchant_discounts_controller.rb
@@ -10,7 +10,6 @@ class MerchantDiscountsController < ApplicationController
   def create
     merchant = Merchant.find(params[:merchant_id])
     if discount_params[:percentage].to_f >= 1
-      require 'pry'; binding.pry
       redirect_to new_merchant_discount_path(merchant), notice: 'Perchantage must be entered as a decimal value less than one'
     else
       merchant.discounts.create!(discount_params)
