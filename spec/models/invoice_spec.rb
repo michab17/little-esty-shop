2ba943847e2313792c62e567f795983ee84b76d7
Micha Bernhard <mib70@pitt.edu>
2021-11-13 14:21:28 -0700

    Test: write discounted_revenue

--- spec/models/invoice_spec.rb
+++ spec/models/invoice_spec.rb
@@ -37,4 +37,19 @@ RSpec.describe Invoice, type: :model do
       expect(invoice2.total_revenue).to eq(32)
     end
   end
+
+  describe '#discounted_revenue' do
+    it 'returns the total revenue including discounts' do
+      merchant = Merchant.create!(name: 'Max Holloway')
+      discount = merchant.discounts.create!(percentage: 0.20, quantity: 2)
+      customer = Customer.create!(first_name: "Grandpa", last_name: "Steve")
+      invoice = Invoice.create!(customer_id: customer.id, status: 'in progress')
+      item1 = Item.create!(name: 'book', description: 'good book', unit_price: 12, merchant_id: merchant.id)
+      item2 = Item.create!(name: 'spatula', description: 'good spatula', unit_price: 8, merchant_id: merchant.id)
+      invoice_item1 = InvoiceItem.create!(item_id: item1.id, invoice_id: invoice.id, quantity: 2, unit_price: 100, status: 'pending')
+      invoice_item2 = InvoiceItem.create!(item_id: item2.id, invoice_id: invoice.id, quantity: 1, unit_price: 20, status: 'shipped')
+
+      expect(invoice.discounted_revenue).to eq('$1.80')
+    end
+  end
 end
