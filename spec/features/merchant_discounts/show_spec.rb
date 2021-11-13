7935d6dbb8dd97f894153998c05f925e06846cc8
Micha Bernhard <mib70@pitt.edu>
2021-11-13 12:29:04 -0700

    Test: write user story 5 tests

---
+++ spec/features/merchant_discounts/show_spec.rb
@@ -0,0 +1,14 @@
+require 'rails_helper'
+
+RSpec.describe 'Merchant Discounts Show Page' do
+  it 'shows the discounts percentage and quantity' do
+    merchant = Merchant.create!(name: 'Freddy Kruger')
+    discount1 = Discount.create!(merchant_id: merchant.id, percentage: 0.25, quantity: 3)
+
+    visit merchant_discount_path(merchant, discount1)
+
+    expect(page).to have_content("Discount #1")
+    expect(page).to have_content("Percentage: 25%")
+    expect(page).to have_content("Quantity: 3")
+  end
+end
