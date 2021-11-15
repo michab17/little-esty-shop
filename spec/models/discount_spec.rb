80dd858ded913a29070a6a91499a1d0b37ced743
Micha Bernhard <mib70@pitt.edu>
2021-11-15 13:57:40 -0700

    Fix test

--- spec/models/discount_spec.rb
+++ spec/models/discount_spec.rb
@@ -2,4 +2,13 @@ require 'rails_helper'
 
 RSpec.describe Discount do
   it { should belong_to :merchant }
+
+  describe '#percent' do
+    it 'returns the percentage value as a percent' do
+      merchant = Merchant.create!(name: 'merchant')
+      discount = merchant.discounts.create!(percentage: 0.20, quantity: 3)
+
+      expect(discount.percent).to eq('20%')
+    end
+  end
 end
