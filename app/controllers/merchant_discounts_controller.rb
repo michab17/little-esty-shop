8b79e61764f58d686ba3abcb8a737362834657ae
Micha Bernhard <mib70@pitt.edu>
2021-11-16 19:05:59 -0700

    Feat: Make sure only one discount appears

--- app/controllers/merchant_discounts_controller.rb
+++ app/controllers/merchant_discounts_controller.rb
@@ -4,7 +4,7 @@ class MerchantDiscountsController < ApplicationController
   end
   
   def show
-    @merchant = Merchant.find(params[:merchant_id])
+    @discount = Discount.find(params[:id])
   end
   
   def edit
