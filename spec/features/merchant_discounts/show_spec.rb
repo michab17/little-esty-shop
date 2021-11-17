485eedfbcbb90d01b39aace9fccb28cd2d0dad5f
Micha Bernhard <mib70@pitt.edu>
2021-11-16 19:05:26 -0700

    Test: add tests to expect only one discount

--- spec/features/merchant_discounts/show_spec.rb
+++ spec/features/merchant_discounts/show_spec.rb
@@ -4,11 +4,15 @@ RSpec.describe 'Merchant Discounts Show Page' do
   it 'shows the discounts percentage and quantity' do
     merchant = Merchant.create!(name: 'Freddy Kruger')
     discount1 = Discount.create!(merchant_id: merchant.id, percentage: 0.25, quantity: 3)
+    discount2 = Discount.create!(merchant_id: merchant.id, percentage: 0.30, quantity: 5)
 
     visit merchant_discount_path(merchant, discount1)
 
+    expect(page).to have_content("Discount #").once
     expect(page).to have_content("Discount ##{discount1.id}")
     expect(page).to have_content("Percentage: 25%")
     expect(page).to have_content("Quantity: 3")
+    expect(page).to_not have_content("Percentage: 30%")
+    expect(page).to_not have_content("Quantity: 5")
   end
 end
