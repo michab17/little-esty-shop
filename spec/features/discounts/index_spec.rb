d143ac21d23d1ed4ac852b1ec2b8b5bda5afcdf5
Micha Bernhard <mib70@pitt.edu>
2021-11-16 18:39:16 -0700

    Test: if date appears on page

--- spec/features/discounts/index_spec.rb
+++ spec/features/discounts/index_spec.rb
@@ -3,11 +3,11 @@ require 'rails_helper'
 RSpec.describe 'Discounts Index Page' do
   it 'displays the next 3 upcoming holidays' do
     visit discounts_path
-
+    save_and_open_page
     within "#upcoming_holidays" do
-      expect(page).to have_content('Thanksgiving Day')
-      expect(page).to have_content('Christmas Day')
-      expect(page).to have_content("New Years Day")
+      expect(page).to have_content('Thanksgiving Day - 2021-11-25')
+      expect(page).to have_content('Christmas Day - 2021-12-24')
+      expect(page).to have_content("New Years Day - 2021-12-31")
       expect(page).to_not have_content("Martin Luther King, Jr. Day")
     end
   end
